d77d8b2e4bf3dd92590932852e73918a864923c4
Mike Ishida <mike@visualhawaii.com>
2017-09-08 16:30:38 -1000

    Addded new comments on User and Product class.

--- MySampleWebApi/Resources/Product.cs
+++ MySampleWebApi/Resources/Product.cs
@@ -7,10 +7,29 @@ namespace MySampleWebApi.Resources
 {
     public class Product
     {
+        /// <summary>
+        /// 商品のIdentity
+        /// </summary>
         public long id { get; set; }
+
+        /// <summary>
+        /// 商品コード(カタログ上のコード)
+        /// </summary>
         public string ProdCd { get; set; }
+
+        /// <summary>
+        /// 商品名称
+        /// </summary>
         public string ProdName { get; set; }
+
+        /// <summary>
+        /// 現在の価格
+        /// </summary>
         public decimal Price { get; set; }
+
+        /// <summary>
+        /// 登録日
+        /// </summary>
         public DateTime CreatedAt { get; set; }
 
     }
--- MySampleWebApi/Resources/User.cs
+++ MySampleWebApi/Resources/User.cs
@@ -7,9 +7,25 @@ namespace MySampleWebApi.Resources
 {
     public class User
     {
+        /// <summary>
+        /// ユーザーのIdentity
+        /// </summary>
         public long id { get; set; }
+
+        /// <summary>
+        /// ユーザー名
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// ユーザーのメールアドレス
+        /// 各種通知メッセージがこのアドレスに送られる。
+        /// </summary>
         public string Email { get; set; }
+
+        /// <summary>
+        /// 登録日時
+        /// </summary>
         public DateTime CreatedAt { get; set; }
 
     }
